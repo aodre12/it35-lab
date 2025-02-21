93cbe8705922f7b0ca27affad5b3856dc0941412
aodre12 <audreyabigailhisanza@gmail.com>
2025-02-21 20:31:06 +0800

    added Details.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,28 @@
 import { 
-  IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Home: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonButton routerLink="/it35-lab/app/home/details" expand="full">
-              Info
-          </IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Home;
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
+} from '@ionic/react';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
